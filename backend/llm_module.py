e4d38fc69658ae8a2950e662e9b08f90577209bf
Nathan Romeo <nromeo8@gmail.com>
2026-04-07 19:28:17 +1000

    Implemented conversational pipeline

--- backend/llm_module.py
+++ backend/llm_module.py
@@ -1,18 +1,12 @@
-# llm_module.py
-
 import requests
+import json
 
-# Local Ollama API used to send prompts to the LLM
 OLLAMA_URL = "http://localhost:11434/api/generate"
-
-# LLM Model
 MODEL_NAME = "qwen2.5:32b"
 
-
+# Send a prompt to Ollama API and return the response.
 def llm_request(prompt):
-
-# Sends a prompt to the Ollama API and returns the model's response text.
-
+    
     response = requests.post(
         OLLAMA_URL,
         json={
@@ -21,64 +15,150 @@ def llm_request(prompt):
             "stream": False,
             "options": {
                 "temperature": 0.1,
-                "num_predict": 300
+                "num_predict": 150
             }
-        }
+        },
+        timeout=180
     )
 
     data = response.json()
-    return data.get("response", "")
+    return data.get("response", "").strip()
 
-
-def translate_text(text):
-# Uses the LLM to translate input text into English.
-# The prompt strictly instructs the model to return only the translated sentence.
-
-        
+# Detect the language of the input text and translate it to English.
+def translate_and_detect_language(text):
+    
     prompt = f"""
-Translate the following text to English.
-Only return the translated sentence.
-Do not include explanations.
-Do not include markdown.
-Do not include extra text.
-Do not include any notes.
+You are a language detection and translation assistant.
 
-Text: "{text}"
+Detect the language of the following text and translate it into English.
+
+Return ONLY valid JSON in the format:
+{{
+  "detected_language": "ISO 639-1 code",
+  "translated_text": "English translation"
+}}
+
+If the text is already in English, return "en" and the original text.
+
+Text:
+\"\"\"{text}\"\"\"
 """
-    return llm_request(prompt)
+    response = llm_request(prompt)
+
+    try:
+        return json.loads(response)
+    # Default to unknown on error
+    except json.JSONDecodeError:
+        return {
+            "detected_language": "unknown",
+            "translated_text": text
+        }
+
+# Determines whether additional information is required and generates a follow up response.
+def generate_follow_up_questions(text):
+
+    prompt = f"""
+You are an AI compliance assistant for an aged care and NDIS provider. 
+You receive case notes from support workers caring for participants.
+
+You are provided with the full conversation between a support worker and an AI assistant.
+Determine whether additional information is required to complete a compliant case report.
+
+ONLY ask a follow-up question if essential information is missing.
+
+Strict Instructions:
+- You only need enough information to define the following:
+    Reporting Type:
+    - incident
+    - feedback_complaint
+    - case_note
+
+    Category:
+    - health
+    - behaviour
+    - administrative
+    - other
+
+    Severity Level:
+    - high, medium, low, none
+
+- Only ask ONE question at a time.
+- Do NOT ask a question if all information can be reasonably determined - return an empty response.
+- Limit the number of questions you ask, preferably to NO MORE than TWO QUESTIONS.
+- Do NOT repeat questions already asked.
+- Do NOT ask questions that have already been answered.
+- Do NOT ask unnecessary clarifying questions.
+- Do NOT ask follow up questions if enough information has been collected for a case note.
+- If the user replies with a one word response (ie. 'yes' or 'no'), you should not ask a follow up question.
+
+Guidelines:
+- If no harm is mentioned, assume severity is NONE.
+- Only ask about medical care if there is a possible injury and it has not been clarified.
+- If enough information is available, return an empty response.
+- You should return the follow up question in the same language as the user's message. 
+
+Return ONLY the question text with no explanations, no JSON, and no extra formatting (including quotation marks).
+
+Conversation History:
+\"\"\"{text}\"\"\"
+"""
+    response = llm_request(prompt).strip()
+
+    # Clean and validate response
+    if not response:
+        return ""
+
+    response = response.strip().strip('"').strip("'")
+
+    # Ensure the output is a valid question
+    if response.endswith("?") and len(response) > 5:
+        return response
+
+    return ""
 
 
-
+# uses LLM to classify a case note and extract structured JSON data for case note.
 def extract_data(text):
 
-# Uses the LLM to classify a case note and extract structured compliance data.
-# The model must return a JSON object with incident classification fields.
-
-
     prompt = f"""
-You are a clinical compliance assistant.
+You are an AI compliance assistant for an aged care and NDIS provider. You receive case notes from support workers that are caring for their clients (referred to as "participants").
 
-Return ONLY valid JSON.
-Do not include explanations.
-Do not include markdown.
-Do not include extra text.
-ALL JSON fields must be filled.
 
-If there is any injury to the client/participant, the minimum 'severity' category should be medium.
-Any hospitalisation should be classified as 'high' and 'escalation_required' should be 'yes'.
+Analyse the case note and extract structured information.
 
+Instructions:
+- Do NOT generate follow-up questions.
+- Assume all required information has already been collected.
+- Hospitalisation or serious injury automatically implies:
+  - severity: high
+  - escalation_required: yes
+
+Return ONLY valid JSON. Do not include explanations, markdown, or extra text.
+All fields must contain a value.
+
+Reporting Types:
+- incident
+- feedback_complaint
+- case_note
+
+Categories:
+- health
+- behaviour
+- administrative
+- other
+
+Severity Levels:
+- high, medium, low, none
 
 Required JSON format:
 {{
-  "incident": "yes or no",
-  "category": "health, behaviour, admin, other",
-  "severity": "low, medium, high",
-  "escalation_required": "yes or no"
+  "report_type": "incident | feedback_complaint | case_note",
+  "category": "health | behaviour | administrative | other",
+  "severity": "low | medium | high | none",
+  "escalation_required": "yes | no"
 }}
 
-Case note:
----START---
-{text}
----END---
+Case Note:
+\"\"\"{text}\"\"\"
 """
     return llm_request(prompt)
--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,186 @@
 from whisper_module import transcribe_audio
-from llm_module import translate_text, extract_data
+from llm_module import translate_and_detect_language,extract_data, generate_follow_up_questions
+from langdetect import detect_langs, DetectorFactory, LangDetectException
+from datetime import datetime
+import uuid
+import json
+
+DetectorFactory.seed = 0 # Set seed for langdetect
+
+# Get current timestamp
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Use langdetect to quickly check language
+def detect_text_language(text):
+
+    # Default to english if short response to stop misclassification
+    if len(text) < 15:
+        return "en", 1.0
+    
+    # Detect language and return ISO code and confidence
+    try:
+        result = detect_langs(text)[0]
+        return result.lang, result.prob
+    except (LangDetectException, IndexError):
+        return "unknown", 0.0
+
+# Conversation history to pass to LLM
+def conversation_history(conversation_log):
+    text = ""
+    for entry in conversation_log:
+        text += entry["role"] + ": " + entry["message"] + "\n"
+    return text.strip()
+
+# Process user input and update the case record
+def process_input(user_input, is_voice_input, case_record):
+    timestamp = get_timestamp()
+
+    # Determine input type
+    if is_voice_input:
+        original_text, lang, confidence = transcribe_audio(user_input) # Pass to faster-whisper to transcribe audio
+    else:
+        original_text = user_input
+        lang, confidence = detect_text_language(original_text)
+
+    print("\nDetected language:", lang)
+    print("Confidence:", round(confidence, 3))
+    print("\nOriginal Transcript:")
+    print(original_text)
+
+    # Translate if the text is not English
+    if lang.lower() != "en":
+        result = translate_and_detect_language(original_text) # Use LLM to detect language and translate
+        english_text = result.get("translated_text", original_text)
+        detected_language = result.get("detected_language", lang)
+    else:
+        english_text = original_text
+        detected_language = "en"
+
+    print("\nEnglish Version:")
+    print(english_text)
+
+    # Store detected language once
+    case_record.setdefault("original_language", detected_language)
+
+    # Log the interaction
+    case_record["conversation_log"].append({
+        "role": "user",
+        "message": original_text,
+        "timestamp": timestamp
+    })
+
+    # Accumulate full original conversation text
+    case_record["full_original_text"] = (case_record.get("full_original_text", "") + " " + original_text).strip()
+
+    # Accumulate full translated conversation text
+    case_record["full_translated_text"] = (case_record.get("full_translated_text", "") + " " + english_text).strip()
+
+    # Update last modified time
+    case_record["last_updated"] = timestamp
+
+    return case_record
+
+# Conversation pipeline - runs until no follow up questions are required
+def conversational_pipeline(user_input, is_voice_input, case_record):
+
+    while True: # Run until no more follow up questions
+        case_record = process_input(user_input, is_voice_input, case_record) # Process user input (transcribe and translate if required)
+
+        history = conversation_history(case_record["conversation_log"]) # Update conversation history
+        follow_up = generate_follow_up_questions(history) # Pass to LLM to generate follow up question if required
+
+        # Stop if no valid follow-up question
+        if not follow_up:
+            return case_record
+
+        follow_up = follow_up.strip().strip('"').strip("'")
+
+        # Validate question format
+        if not follow_up.endswith("?") or len(follow_up) < 5:
+            return case_record
+
+        # Log assistant question
+        case_record["conversation_log"].append({
+            "role": "assistant",
+            "message": follow_up,
+            "timestamp": get_timestamp()
+        })
+
+        print("\nFollow Up Question:")
+        print(follow_up)
+
+        # Hard coded to text input for now
+        is_voice_input = False
+        user_input = input("Enter your response: ").strip()
+
+# Generated final structured case note in JSON format
+def final_extraction(case_record):
+  
+    print("\nAll required information collected. Generating final report...\n")
+
+    final_text = case_record.get("full_translated_text", "").strip()
+    structured_output = extract_data(final_text)
+
+    # Display raw LLM output
+    print("LLM Output:")
+    print(structured_output)
+
+    # Parse JSON from LLM
+    try:
+        structured_data = json.loads(structured_output)
+    # Default to 'unknown' if error with JSON
+    except json.JSONDecodeError: 
+        structured_data = {
+            "report_type": "unknown",
+            "category": "unknown",
+            "severity": "unknown",
+            "escalation_required": "unknown"
+        }
+
+    escalation = structured_data.get("escalation_required", "no") # Default escalation to 'no' if not specified 
+
+    # If conversation is in english, do not store translated text
+    if case_record["full_original_text"].lower() == case_record["full_translated_text"].lower():
+        case_record["full_translated_text"] = ""
+
+    # Update case record with final details
+    case_record.update({
+        "status": "escalated" if escalation == "yes" else "in_progress",
+        "report_type": structured_data.get("report_type"),
+        "category": structured_data.get("category"),
+        "severity": structured_data.get("severity"),
+        "escalation_required": escalation,
+        "last_updated": get_timestamp()
+    })
+
+    return case_record
 
 
 if __name__ == "__main__":
+    print("CareShield AI Prototype")
+    print("-----------------------")
 
-    audio_file = "sample.mp3"
+    
+    is_voice_input = False # Hard coded for testing
+    user_input = input("Enter case note: ").strip()
 
-    # Transcribe
-    original_text, detected_language, confidence = transcribe_audio(audio_file)
+    # Initialise case_id
+    case_id = uuid.uuid4().hex[:8].upper()
 
-    print("\nDetected language:", detected_language)
-    print("Confidence:", round(confidence, 3))
-    print("\n--- Original Transcript ---")
-    print(original_text)
+    # Initialise case record
+    case_record = {
+        "case_id": case_id,
+        "name": f"user-{case_id}",  # hard coded until login implemented
+        "email": f"{case_id}@email.com", # hard coded until login implemented
+        "role": "user", # hard coded until login implemented
+        "status": "in_progress",
+        "created_at": get_timestamp(),
+        "conversation_log": []
+    }
 
-    # Translate if needed
-    if detected_language != "en":
-        english_text = translate_text(original_text)
-    else:
-        english_text = original_text
+    case_record = conversational_pipeline(user_input, is_voice_input, case_record) # Run conversation pipeline
+    final_case = final_extraction(case_record)    # Extract data into case note
 
-    print("\n--- English Version ---")
-    print(english_text)
-
-    # Classify
-    structured_output = extract_data(english_text)
-
-    print("\n--- Structured Output ---")
-    print(structured_output)
+    print("\nFinal Case Record:")
+    print(json.dumps(final_case, indent=4, ensure_ascii=False))
